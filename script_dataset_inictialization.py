3f803fd882581e534ffbe626d888d950abc30120
Luizfrf3 <lf.fonseca@hotmail.com>
2016-11-11 15:10:46 -0200

    Habemos Frasiuns

--- script_dataset_inictialization.py
+++ script_dataset_inictialization.py
@@ -3,25 +3,21 @@ from py2neo import *
 g = Graph(password="123456")
 
 sentiment_labels = "stanfordSentimentTreebank/stanfordSentimentTreebank/sentiment_labels.txt" 
+dictionary = "stanfordSentimentTreebank/stanfordSentimentTreebank/dictionary.txt"
 
-file = open(sentiment_labels)
-lines = [line for line in file]
+f_sentiment = open(sentiment_labels)
+sentiments = [line for line in f_sentiment]
+
+f_dictionary = open(dictionary)
+d = {}
+
+for line in f_dictionary:
+	s = line.split('|')
+	d[int(s[1])] = s[0]
+
 for i in range(1,50):
-	line = lines[i]
+	line = sentiments[i]
 	x = tuple(line.split('\n')[0].split('|'))
-	query = "create (:frases{id:%s, val:%s})" % x
+	query = "create (:frases{id:%s, val:%s, frase:{f}})" % x
 	print(query)
-	g.run(query)
-
-datasetSentences = "stanfordSentimentTreebank/stanfordSentimentTreebank/datasetSentences.txt"
-
-file = open(datasetSentences)
-lines = [line for line in file]
-
-for i in range(1,50):
-	line = lines[i]
-	[id,frase]= line.split('\n')[0].split('\t')
-	query = "match (f:frases) where f.id = {id} set f.frase = {frase}"
-	print(frase)
-	print("\n")
-	g.run(query, id = id, frase = frase)
+	g.run(query, f = d[int(x[0])])
