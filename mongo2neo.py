ef421d6baf018b05cd35056220639f05f819fdfb
LRacoci <racoci.0@gmail.com>
2016-11-17 21:16:22 -0200

    teste

--- mongo2neo.py
+++ mongo2neo.py
@@ -4,6 +4,15 @@ from py2neo import *
 m = pymongo.MongoClient()
 c = m.twitter.tweets
 
-conta = c.count({ 'lang': 'en', '$or': [ { 'geo': { '$ne':'null' } }, { 'place': { '$ne':'null' } }, { 'coordinates': { '$ne':'null' } } ] })
+query = { 
+	'lang': 'en', 
+	'$or': [ 
+		{ 'geo': { '$ne': 'null' } }, 
+		{ 'place': { '$ne':'null' } }, 
+		{ 'coordinates': { '$ne':'null' } }
+	]
+}
+
+conta = c.find(query).count()
 
 print conta 
