4b088a6fb7668a1182ce8b9002e260799d8fb211
Luiz Fernando <lf.fonseca@hotmail.com>
2016-11-18 13:48:26 -0200

    Mais testes com as localizacoes

--- mongo2neo.py
+++ mongo2neo.py
@@ -14,8 +14,31 @@ query = {
 	] 
 }
 
-cursor = c.find(query, limit=20)
+cursor = c.find(query, limit=100)
 
+geolocator = Nominatim()
+
+cont = 0
 for tweet in cursor:
+	place = tweet['place']
 	coordinates = tweet['coordinates']
-	print tweets
+	location = None
+	if coordinates != None:
+		coordinates = coordinates['coordinates']
+		location = geolocator.reverse("%f, %f" % (coordinates[1], coordinates[0]))		
+	flag = 0
+	if location != None and place != None:
+		country = place['country']
+		if country == "United States":
+			cont += 1
+			flag = 1
+	if location != None:
+		country = location.raw['address']['country']
+		if flag == 0 and country == "United States of America":
+			cont += 1
+			flag = 1
+	if place != None:
+		country = place ['country']
+		if flag == 0 and country == "United States":
+			cont += 1
+print cont
