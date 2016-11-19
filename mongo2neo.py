dcb1de29878bcaafb73d5a602d0add6d87a82763
Luizfrf3 <lf.fonseca@hotmail.com>
2016-11-19 15:40:57 -0200

    Unavailable service

--- mongo2neo.py
+++ mongo2neo.py
@@ -1,7 +1,7 @@
 import pymongo
 from py2neo import *
 from geopy import Nominatim
-from geopy.exc import GeocoderTimedOut, GeocoderQuotaExceeded
+from geopy.exc import GeocoderTimedOut, GeocoderQuotaExceeded, GeocoderUnavailable
 import time
 from py2neo.packages.httpstream import http
 http.socket_timeout = 9999
@@ -23,51 +23,60 @@ cursor = c.find(query, no_cursor_timeout = True)
 
 geolocator = Nominatim(timeout = 5)
 
+i = 1
 for tweet in cursor:
-	place = tweet['place']
-	coordinates = tweet['coordinates']
-	location = None
+	if i > 1276:
+		place = tweet['place']
+		coordinates = tweet['coordinates']
+		location = None
 
-	if coordinates != None:
-		lon = coordinates['coordinates'][0]
-		lat = coordinates['coordinates'][1]
-		if -170 < lon < -60 and 12 < lat < 72:
-			tries = 3
-			while tries > 0:
-				try:
-					time.sleep(1)
-					location = geolocator.reverse("%f, %f" % (lat, lon))
-					break
-				except GeocoderTimedOut as e:
-					print "GeocoderTimedOut"
-					time.sleep(1)
-					tries -= 1
-				except GeocoderQuotaExceeded as e:
-					print "QuotaExceeded"
-					time.sleep(10)
+		if coordinates != None:
+			lon = coordinates['coordinates'][0]
+			lat = coordinates['coordinates'][1]
+			if -170 < lon < -60 and 12 < lat < 72:
+				tries = 3
+				while tries > 0:
+					try:
+						time.sleep(1)
+						location = geolocator.reverse("%f, %f" % (lat, lon))
+						break
+					except GeocoderTimedOut as e:
+						print "GeocoderTimedOut"
+						time.sleep(1)
+						tries -= 1
+					except GeocoderQuotaExceeded as e:
+						print "QuotaExceeded"
+						time.sleep(10)
+					except GeocoderUnavailable as e:
+						print "GeocoderUnavailable"
+						time.sleep(30)
 
-	elif place != None:
-		place_coordinates = place['bounding_box']['coordinates']
-		lon = (float(place_coordinates[0][0][0]) + float(place_coordinates[0][1][0])) / 2
-		lat = (float(place_coordinates[0][0][1]) + float(place_coordinates[0][2][1])) / 2
-		if -170 < lon < -60 and 12 < lat < 72 and place['country'] == "United States":
-			tries = 3
-			while tries > 0:
-				try:
-					time.sleep(1)
-					location = geolocator.reverse("%f, %f" % (lat, lon))
-					break
-				except GeocoderTimedOut as e:
-					print "GeocoderTimedOut"
-					time.sleep(1)
-					tries -= 1
-				except GeocoderQuotaExceeded as e:
-					print "QuotaExceeded"
-					time.sleep(10)
+		elif place != None:
+			place_coordinates = place['bounding_box']['coordinates']
+			lon = (float(place_coordinates[0][0][0]) + float(place_coordinates[0][1][0])) / 2
+			lat = (float(place_coordinates[0][0][1]) + float(place_coordinates[0][2][1])) / 2
+			if -170 < lon < -60 and 12 < lat < 72 and place['country'] == "United States":
+				tries = 3
+				while tries > 0:
+					try:
+						time.sleep(1)
+						location = geolocator.reverse("%f, %f" % (lat, lon))
+						break
+					except GeocoderTimedOut as e:
+						print "GeocoderTimedOut"
+						time.sleep(1)
+						tries -= 1
+					except GeocoderQuotaExceeded as e:
+						print "QuotaExceeded"
+						time.sleep(10)
+					except GeocoderUnavailable as e:
+						print "GeocoderUnavailable"
+						time.sleep(30)
 
-	if location != None:
-		if 'address' in location.raw.keys() and 'state' in location.raw['address'].keys():
-			tweet_node = Node("tweet", text = tweet['text'], user_id = tweet['user']['id_str'], label = 0.5, coordinates = [lat, lon], state = location.raw['address']['state'], created_at = tweet['created_at'])
-			g.create(tweet_node)
-		else:
-			print "Erro address/state"
+		if location != None:
+			if 'address' in location.raw.keys() and 'state' in location.raw['address'].keys():
+				tweet_node = Node("tweet", text = tweet['text'], user_id = tweet['user']['id_str'], label = 0.5, coordinates = [lat, lon], state = location.raw['address']['state'], created_at = tweet['created_at'])
+				g.create(tweet_node)
+			else:
+				print "Erro address/state"
+	i += 1
