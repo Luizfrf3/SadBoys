aabb95bea78b10a245fd3a64a43ad7b93533b5df
Luizfrf3 <lf.fonseca@hotmail.com>
2016-11-24 13:51:42 -0200

    Usuarios quase prontos para serem criados

--- BD/mongoUsers2neo.py
+++ BD/mongoUsers2neo.py
@@ -4,28 +4,24 @@ from py2neo import *
 d = MongoClient().twitter.users
 g = Graph(bolt = False, password = "neo4j")
 
-cursor = c.find()
+cursor = d.find()
 
 # Insere todos os usuários primeiro
 for user in cursor:
-	userNode = Node(
-		"user", 
-		name = user[u'name'],
-		id = user[u'id_str'],
-		profile_image = user[u'profile_image_url']
-	)
-	g.create(userNode)
+	s = user['id_str']
+	query = "match (t:tweet) where t.user_id = '%s' return count(t) as n" % s
+	c = g.run(query)
+	if c['n'] > 0:
+		userNode = Node(
+			"user", 
+			name = user[u'name'],
+			id = user[u'id_str'],
+			friends = user[u'friends']
+			followers = user[u'followers']
+			label = 0.5
+		)
+		g.create(userNode)
 
-query = "MATCH (t:tweet) MATCH (u:user) WHERE u.id = t.user_id CREATE (u)-[:make]->(t);"
+query = "match (t:tweet), (u:user) where u.id = t.user_id create (u)-[:twitted]->(t)"
 
-g.run(query) # Acho que é assim
-
-# Agora, se ainda for necessário, deleta os nós que não ligaram com ninguém, mas isso parece muito arriscado
-
-
-
-
-
-
-
-
+g.run(query)
