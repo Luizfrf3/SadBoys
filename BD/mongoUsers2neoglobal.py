d1c2a4b0bbbf09ca4d6bc36fecf2657cafbdb2ea
Luizfrf3 <lf.fonseca@hotmail.com>
2016-11-24 23:41:34 -0200

    Teste

---
+++ BD/mongoUsers2neoglobal.py
@@ -0,0 +1,44 @@
+from pymongo import MongoClient
+from py2neo import *
+
+d = MongoClient().twitter.users
+
+#g = Graph(password = "123456")
+g = Graph(bolt = False, password = "neo4j")
+
+query = "match (t:tweet) return collect(distinct t.user_id) as ids"
+
+idlist = g.run(query).next()['ids']
+idset = set(str(x) for x in idlist)
+
+query = {
+	'id_str': { '$in': idlist }
+}
+
+cursor = d.find(query)
+
+i = 1
+for user in cursor:
+	userNode = Node(
+		"user", 
+		name = user['name'],
+		id = user['id_str'],
+		profile_image = user['profile_image_url'],
+		label = 0.5
+	)
+	g.create(userNode)
+	print i
+	i += 1
+
+query = "match (t:tweet) match (u:user) where u.id = t.user_id create (u)-[:twitted]->(t)"
+
+g.run(query)
+
+query = {
+	'id_str': { '$in': idlist }
+}
+
+cursor = d.find(query)
+
+for user in cursor:
+	g.run("match (u1:user) match (u2:user) where u1.id = {uid} and u2.id in {followers} create (u2)-[:follows]->(u1)", uid = user['id_str'], followers = [str(x['id']) for x in user['followers']])
