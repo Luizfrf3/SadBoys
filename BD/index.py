9d584a18f671ebbbc0836987121b9f9b1b023a32
Luizfrf3 <lf.fonseca@hotmail.com>
2016-11-26 12:15:07 -0200

    Teste

--- BD/index.py
+++ BD/index.py
@@ -2,11 +2,6 @@ from py2neo import *
 
 g = Graph(bolt = False, password = "neo4j")
 
-g.schema.drop_indexes("tweet")
-g.schema.drop_indexes("user")
-g.schema.drop_indexes("tweetglobal")
-g.schema.drop_indexes("userglobal")
-
 g.schema.create_index("tweet", "text")
 g.schema.create_index("tweet", "user_id")
 g.schema.create_index("tweet", "coordinates")
