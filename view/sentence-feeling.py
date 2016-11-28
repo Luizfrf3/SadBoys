44db3b963e65a8c96f5a931c4db351d539ba6c44
henriquefacioli <henrique.facioli@gmail.com>
2016-11-28 18:18:59 -0200

    To deal with feeling

---
+++ view/sentence-feeling.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+# Module to deal with an input from the user and analyze its feeling
+# Authors: Henrique
+#          Thiago
+
+import random, json, sys
+
+sys.path.insert(0, '../')
+from sent_anaysis.apply_ import graph_dataset
