4e7bfde38869ed69699e0fa8cf9e6461f72e4df4
Tsfarias <t148077@dac.unicamp.br>
2016-11-28 12:17:18 -0200

    Vamo que vamo

--- view/app.py
+++ view/app.py
@@ -6,6 +6,7 @@ from flask import render_template
 from flask import jsonify, json, Response
 
 from graph import generate_graph, generate_edges, generate_nodes, initial_graph, user_graph
+from map import initial_heatmap, create_state_rates, create_tweets
 
 app = Flask(__name__)
 
@@ -31,6 +32,11 @@ def graph_complete_user(user_id):
 @app.route("/map")
 def map():
     return render_template("map.html")
+
+@app.route("/map/data")
+def map_states():
+    s, t = initial_heatmap()
+    return jsonify(states = s, tweets = t)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True)
---
+++ view/map.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+# Module to deal with the graph data
+# Authors: Henrique
+#          Thiago
+
+import random, json, sys
+
+sys.path.insert(0, '../')
+from BD.heatmap_dataset import heatmap_dataset
+
+state_names = [
+    "Alabama",
+    "Alaska",
+    "Arizona",
+    "Arkansas",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "Delaware",
+    "District of Columbia",
+    "Florida",
+    "Georgia",
+    "Hawaii",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Iowa",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Maine",
+    "Maryland",
+    "Massachusetts",
+    "Michigan",
+    "Minnesota",
+    "Mississippi",
+    "Missouri",
+    "Montana",
+    "Nebraska",
+    "Nevada",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "New York",
+    "North Carolina",
+    "North Dakota",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Vermont",
+    "Virginia",
+    "Washington",
+    "West Virginia",
+    "Wisconsin",
+]
+
+def initial_heatmap():
+    # Creates a new object to deal with bd
+    db = heatmap_dataset()
+    initial_tweets = db.getTweets()
+
+
+    states = []
+    tweets = []
+
+    for state in state_names:
+        s_rates = db.getStateData(state)
+        states.append(create_state_rates(s_rates['name'], s_rates['avg_label'],
+                                         s_rates['suicide_rate'], s_rates['depressive_percentage'],
+                                         s_rates['suicide_percentage']))
+
+    for tweet in initial_tweets:
+        tweets.append(create_tweets(tweet['coord'], tweet['label']))
+
+    return states, tweets
+
+# Creates states_rates
+def create_state_rates(name, avg_label, suicide_rate, depressive_percentage, suicide_percentage):
+    state_rates = dict()
+
+    state_rates['name'] = str(name)
+    state_rates['avg_label'] = float(avg_label)
+    state_rates['suicide_rate'] = float(suicide_rate)
+    state_rates['depressive_percentage'] = float(depressive_percentage)
+    state_rates['suicide_percentage'] = float(suicide_percentage)
+
+    return state_rates
+
+# Creates tweets
+def create_tweets(coord, label):
+    tweets = dict()
+
+    tweets['lat'] = float(coord[0])
+    tweets['long'] = float(coord[1])
+    tweets['label'] = float(label)
+
+    return tweets
