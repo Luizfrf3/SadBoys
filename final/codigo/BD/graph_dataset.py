9cf5020fa87d89e404da80250053d1c31e91f97d
Luizfrf3 <lf.fonseca@hotmail.com>
2016-11-29 01:04:45 -0200

    Diretorio final

---
+++ final/codigo/BD/graph_dataset.py
@@ -0,0 +1,57 @@
+"""
+    Implementacao de API que se comunica com o modulo de visualizacao
+    que mostrara o grafo dos usuarios e suas relacoes na tela
+
+    Autores: Lucas Alves Racoci - RA 156331
+             Luiz Fernando Rodrigues da Fonseca - RA 156475
+
+"""
+
+from py2neo import *
+import numpy as np
+from py2neo.packages.httpstream import http
+
+class graph_dataset:
+	# Inicializa o tempo do socket e o neo4j
+	def __init__(self):
+
+		http.socket_timeout = 9999
+		self.g = Graph(bolt = False, password = "neo4j")
+
+	# Dado o nome unico do usuario, retorna o id dele se existir
+	def getUserID(self, screen_name):
+
+		query = "match (u:userglobal) where u.screen_name = '%s' return u.id as id" % (screen_name)
+		cursor = self.g.run(query)
+
+		data = cursor.data()
+
+		if data == None:
+			return None
+
+		return data[0]['id']
+
+	# Retorna quem um usuario dado pelo id segue e suas informacoes
+	def getFollows(self, user_id):
+
+		query = "match (u:userglobal) where u.id = '%s' return u" % (user_id)
+
+		cursor = self.g.run(query)
+		data = cursor.data()[0]['u']
+
+		query = "match (u1:userglobal)-[:followsglobal]->(u2:userglobal) where u1.id = '%s' return u2.id as id, u2.screen_name as screen_name, u2.label as label" % (user_id)
+
+		cursor = self.g.run(query)
+		follows = cursor.data()
+
+		return data, follows
+
+	# Dado o id de um usuario, retorna os primeiros 500 tweets dele
+	def getTweets(self, user_id):
+
+		query = "match (u:userglobal)-[:twittedglobal]->(t:tweetglobal) where u.id = '%s' return t limit 500" % (user_id)
+
+		cursor = self.g.run(query)
+		data = cursor.data()
+
+		return [x['t'] for x in data]
---
+++ final/codigo/view/app.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+import sys
+
+from flask import Flask
+from flask import render_template
+from flask import jsonify, json, Response, request
+
+from graph import initial_graph, user_graph, user_tweet_graph
+from map import states_heatmap, states_heatmap_complete, tweets_heatmap, tweets_heatmap_happy, tweets_heatmap_sad
+
+sys.path.insert(0, '../')
+import sent_analysis.apply_demo as apde
+
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def view_analysis():
+    if request.method == 'POST':
+        search = str(request.form['search'])
+        av = apde.analyse(str(request.form['search']))
+        print(str(av))
+        return render_template("index.html", av = av, search = search)
+    if request.method == 'GET':
+        return render_template("index.html")
+
+@app.route("/graph")
+def graph():
+    return render_template('graph.html')
+
+@app.route("/graph/data/initial")
+def graph_complete():
+    n, e = initial_graph()
+    return jsonify(nodes = n, edges = e)
+
+# To acess a certain node date just go to graph
+@app.route("/graph/data/follows/<int:user_id>")
+def graph_complete_user_f(user_id):
+    n, e = user_graph(int(user_id))
+    return jsonify(nodes = n, edges = e)
+
+# To acess a certain node date just go to graph
+@app.route("/graph/data/tweets/<int:user_id>")
+def graph_complete_user_t(user_id):
+    n, e = user_tweet_graph(int(user_id))
+    return jsonify(nodes = n, edges = e)
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+@app.route("/map-happy")
+def map_happy():
+    return render_template("map-happy.html")
+
+@app.route("/map-sad")
+def map_sad():
+    return render_template("map-sad.html")
+
+@app.route("/map-statistics")
+def map_statistics():
+    states = states_heatmap()
+    print states
+    return render_template("map-statistics.html", states=states)
+
+@app.route("/map/data/states")
+def map_states():
+    s = states_heatmap()
+    return jsonify(s)
+
+@app.route("/map/data/tweets")
+def map_tweets():
+    t = tweets_heatmap()
+    return jsonify(t)
+
+@app.route("/map/data/tweets/happy")
+def map_tweets_happy():
+    t = tweets_heatmap_happy()
+    return jsonify(t)
+
+@app.route("/map/data/tweets/sad")
+def map_tweets_sad():
+    t = tweets_heatmap_sad()
+    return jsonify(t)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
